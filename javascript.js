6220cf7b6979afb627b8a77bba8348d812f8ba37
pawel4449 <belczykp@gmail>
2025-02-12 10:47:03 +0100

    done human choice, rock, paper, scissors
    to choose you can write numbers, 1,2, 3 or write your choice

--- javascript.js
+++ javascript.js
@@ -27,23 +27,39 @@ game to infinity ? or to some limit = 10 times ?
 
  
 
+// function random by computer one of three values: 0,1 or 2; and transit these value into rock, paper or scissors
 
 function GetComputerChoice(max) {                        
-  let pCchoice= Math.floor(Math.random()*3)    ;
+  let pCchoice= Math.floor(Math.random()*3)+1    ;
 
-if (pCchoice===0) {
+if (pCchoice===1) {
     return "rock";
-} else if (pCchoice===1) {
+} else if (pCchoice===2) {
     return "paper";
 } else {
     return "scissors";
 }                               
-     }                                          
+     }     
+     
+// function human choice, you can write 1,2,3 or rock, paper, scissors.
+function GetHumanChoice(min)   {
+    let hChoice= prompt('What do you choose ? Write 1 or "rock", 2 or "paper" , 3 or "scissors"');           
+    
+        if (hChoice===1||"rock") {  
+    return "rock";}
+          else if (hChoice===2||"paper") {
+    return "paper";}
+ else {
+    return "scissors"};
+}
+
+
+
          
-        
-
-
-
+ console.log(GetHumanChoice(2));
+ 
+   
+  
 
 
 
